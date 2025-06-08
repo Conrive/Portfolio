e7db5fbcb82a3d5174542dd4f25fb3aa91f1a2e7
ksenemZ <manik_1992@list.ru>
2025-06-08 19:38:55 +0300

    Login fix

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24,
     httpOnly: true,
-    secure: false, //Поставить true после получения secure в "https://"
+    secure: true, //Поставить true после получения secure в "https://"
     sameSite: 'strict',
   }
 }));
