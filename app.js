012cb99141be236d517f0fe9ff45d0cf043e38d7
Conrive <ivannaumenko50570@gmail.com>
2025-06-14 13:49:14 +0300

    DB cleaning & UI fixes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24,
     httpOnly: true,
-    secure: true, //Поставить true после получения secure в "https://"
+    secure: false, //Поставить true после получения secure в "https://"
     sameSite: 'strict',
   }
 }));
